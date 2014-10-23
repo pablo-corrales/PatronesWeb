62b33931f64bd7eac841a45fa7f244668b894bb7
pablo-corrales <charliequiro>
2014-10-24 00:17:44 +0200

    Se implementa clase Round

--- src/main/java/models/entities/Round.java
+++ src/main/java/models/entities/Round.java
@@ -1,6 +1,33 @@
 package models.entities;
 
+import java.util.Set;
+
 public class Round {
 	
+	private int id;
+    private Set<User> players;
+    
+    public Round(int id, Set<User> players) {
+        super();
+        this.id = id;
+        this.players = players;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public Set<User> getPlayers() {
+        return players;
+    }
+    
+    public void setPlayers(Set<User> players) {
+        this.players = players;
+    }
+       
 
 }
